a6a4b049d700ef0265f5a649f597c13b0daa1bb9
awenafunagain <awen.bourdon@gmail.com>
2024-07-18 16:14:44 +0200

    tailwind correction

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,8 @@
+"use client"
+
 const Footer = () => {
   return (
    
-
 <footer className="bg-white dark:bg-black">
     <div className="mx-auto w-full max-w-screen-xl">
       <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
--- Components/ScrollingMessage.jsx
+++ Components/ScrollingMessage.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 const ScrollingMessage = () => {
   return (
     <div className="relative flex overflow-x-hidden bg-purple-300 border-t-4 border-solid border-black">
-  <div className="py-12 animate-marquee whitespace-nowrap font-bold text-orange-600">
-    <span className="text-8xl mx-12">EDGE NEWS </span>
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-  </div>
+      <div className="py-12 animate-marquee whitespace-nowrap font-bold text-orange-600">
+        <span className="text-8xl mx-12">EDGE NEWS </span>
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+      </div>
 
-  <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap font-bold text-orange-600">
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-    <span className="text-8xl mx-12">EDGE NEWS</span>
-  </div>
-</div>
+      <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap font-bold text-orange-600">
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+        <span className="text-8xl mx-12">EDGE NEWS</span>
+      </div>
+    </div>
   );
 };
 
